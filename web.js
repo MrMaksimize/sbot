7f752b862f5a2b9b823e317dd7419a32f0a56649
Maksim Pecherskiy <max@maksimize.com>
2015-01-29 01:17:36 -0800

    Adding message

--- web.js
+++ web.js
@@ -58,8 +58,9 @@ app.get('/sms', function(req, res) {
     //return res.json(result)
     //res.send(twiml.toString());
     var firstRec = _.first(result);
+    var resCount = result.length;
     twiml.message('I think you\'re looking for ' + firstRec.contact + '. You can reach them by calling ' + firstRec.phone + ' or going to their site: ' + firstRec.url + '. They are located at ' + firstRec.address + '.');
-    twiml.message('FYI, I may be wrong and dumb for now, but I hope to get smarter every day my benevolent creator, MrMaksimize works on me');
+    twiml.message('FYI - this is off the top of my head, I have ' + (resCount - 1) + ' more potential results, but I\'m still dumb.  I do have big dreams and hope to get smarter soon.');
     // Deploy to keen, return msg:
     keenClient.addEvent('searches', { search: text }, function() {
       return res.send(twiml.toString());
