656836112690f5ec35928f0b786237cf324c9315
Maksim Pecherskiy <max@maksimize.com>
2015-01-29 00:31:35 -0800

    Switching to twiml message.

--- web.js
+++ web.js
@@ -51,8 +51,8 @@ app.get('/sms', function(req, res) {
     //return res.json(result)
     //res.send(twiml.toString());
     var firstRec = _.first(result);
-    twiml.sms('I think you\'re looking for ' + firstRec.contact + '. You can reach them by calling ' + firstRec.phone + ' or going to their site: ' + firstRec.url + '. They are located at ' + firstRec.address + '.');
-    twiml.sms('FYI, I may be wrong and dumb, but I hope to get smarter every day my benevolent creator, MrMaksimize works on me');
+    twiml.message('I think you\'re looking for ' + firstRec.contact + '. You can reach them by calling ' + firstRec.phone + ' or going to their site: ' + firstRec.url + '. They are located at ' + firstRec.address + '.');
+    twiml.message('FYI, I may be wrong and dumb for now, but I hope to get smarter every day my benevolent creator, MrMaksimize works on me');
     //return res.json(firstRec);
     return res.send(twiml.toString());
   });
